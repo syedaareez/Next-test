b1da91841e9719c6c55131c07b281c7e9fcc4efc
Aareez <syedaareez7@gmail.com>
2022-06-09 02:16:18 +0530

    New links

--- components/list.js
+++ components/list.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import Link from 'next/link';
 
 export default function List(){
 
@@ -6,14 +7,8 @@ export default function List(){
 
     useEffect(()=>{
         const myProjectsArray=[
-            {name:"One",details:"Details....!"},
-            {name:"Two",details:"Details....!"},
-            {name:"Three",details:"Details....!"},
-            {name:"Four",details:"Details....!"},
-            {name:"One",details:"Details....!"},
-            {name:"Two",details:"Details....!"},
-            {name:"Three",details:"Details....!"},
-            {name:"Four",details:"Details....!"},
+            {name:"Sorty",details:"Details....!",page:"sorty"},
+            {name:"Three",details:"Details....!",page:"threeD"},
         ]
         SetMyProjects(myProjectsArray);
     },[]);
@@ -21,10 +16,12 @@ export default function List(){
     return(
         <>
         <div className="border-4 flex flex-col w-full items-center overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-gray-100">
-            {myProjects.map((project)=>(
-                <div className="flex w-[80%] p-4 m-4 text-3xl bg-white shadow-md shadow-gray-400 hover:shadow-gray-800 hover:bg-gray-800 hover:text-white cursor-pointer">
-                    {project.name}
-                </div>
+            {myProjects.map((project,id)=>(
+                <Link href={`${project.page}`}>
+                    <div key={id} className="flex w-[80%] p-4 m-4 text-3xl bg-white shadow-md shadow-gray-400 hover:shadow-gray-800 hover:bg-gray-800 hover:text-white cursor-pointer">
+                        {project.name}
+                    </div>
+                </Link>
             ))}
         </div>
         </>
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <div className="grid grid-rows-[1fr_8fr_1fr] h-screen">
         <div className="flex items-center justify-center shadow-sm shadow-gray-400">
-          <h1 className="text-5xl text-center">Welcome to <span className="text-red-800">TEST</span>!</h1>
+          <h1 className="text-5xl text-center">Welcome to <span className="text-red-800">My Projects</span>!</h1>
         </div>
 
         <div className="flex w-full overflow-hidden">
